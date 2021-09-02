5b0d88914f2611a1a8b76b75f5170b7d25f74299
binyamin1629 <binyamindeutschh@gmail.com>
2021-09-02 13:44:56 +0300

    added django heroku 4

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 import django_heroku
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,7 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'chatroom',
     'chats',
-    'dajngo heroku'
+
 ]
 
 MIDDLEWARE = [
